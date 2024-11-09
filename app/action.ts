9ba8af234be6b8ed2eaf5445fea0da416c4ecca0
ChiSamuel651 <chijoe0306@gmail.com>
2024-11-09 15:49:52 +0100

    database configuration 2.0

--- app/action.ts
+++ app/action.ts
@@ -1,6 +1,25 @@
 'use server'
 
-export async function testConsole(){
- console.log("test use server");
+import prisma from "@/lib/prisma"
+
+export async function checkAndAddUser( email: string | undefined ){
+    if(!email) return
+
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: {email}
+        })
+
+        if(!existingUser){
+            await prisma.user.create({
+                data: {email}
+            })
+            console.log("Nouvel utilisateurs ajouter dans la base de données")
+        }else{
+            console.log("Cet utilisateur existe!!")
+        }
+    } catch (error) {
+        console.error("Erreurs lors de la verification de l'utilisateur: ", error)
+    }
     
 }
--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -1,16 +1,8 @@
 'use client'
 import React, { useEffect } from 'react'
-import { testConsole } from '../action';
 
 const page = () => {
-    useEffect(() => {
-        // Wrap the async call inside a function
-        const callTestConsole = async () => {
-            await testConsole();
-        };
-
-        callTestConsole();
-    }, []);
+    
 
   return (
     <div>
